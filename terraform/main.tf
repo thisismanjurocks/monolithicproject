46936a6d676dc35677051fde622955f7011aabcf
Manju Reddy <63736574+thisismanjurocks@users.noreply.github.com>
2023-11-15 13:36:29 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,11 @@
 provider "aws" {
-region = "us-east-1"
+region = "ap-southeast-1"
+access_key = "AKIAQMKZYLPGNFC5IWEV"
+secret_key = "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"
 }
 
 resource "aws_instance" "one" {
-ami = "ami-0e8a34246278c21e4"
+ami = "ami-0ebcd68de1afe59cd"
 instance_type = "t2.micro"
 key_name = "mainkeypair"
 vpc_security_group_ids = [aws_security_group.demo-sg.id]
