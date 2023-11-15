ceb76d76e9d8084ae22bea0320bcd64ed1a2df52
root <root@ip-10-0-0-86.ap-southeast-1.compute.internal>
2023-11-15 06:22:19 +0000

    terrform code is ready

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+region = "us-east-1"
+access_key = "AKIAQMKZYLPGOSJGLEEP"
+secret_key = "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"
+}
+
+resource "aws_instance" "one" {
+ami = ""
+instance_type = "t2.micro"
+vpc_security_group_ids = [aws_security_group.demo-sg.id]
+tags = {
+Name = "project1"
+}
+}
